feb7916de4eb00b24231b61f32d87cf599901a18
magui165-byte <113001565+magui165-byte@users.noreply.github.com>
2025-06-01 02:14:14 -0400

    added api for users - (get) list, (put) update

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -8,6 +8,9 @@ router.get("/user-test", check.auth, UserController.testUser);
 router.post("/register", UserController.registerUser);
 router.post("/login", UserController.loginUser);
 router.get("/profile/:id", check.auth, UserController.profile);
+router.get("/list", check.auth, UserController.list);
+router.get("/list/:page", check.auth, UserController.list);
+router.put("/update", check.auth, UserController.updateUser);
 
 
 
